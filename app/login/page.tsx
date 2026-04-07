916a1bbcdafa045292dc670efd84388a227e7df4
deeyo <deeyo0312@gmail.com>
2026-04-08 00:38:57 +0800

    fix recommendations

---
+++ app/login/page.tsx
@@ -0,0 +1,3 @@
+export default function LoginPage() {
+  return <h1 className="p-8 text-3xl font-bold">登入頁面 (施工中) </h1>;
+}
---
+++ app/register/page.tsx
@@ -0,0 +1,3 @@
+export default function RegisterPage() {
+  return <h1 className="p-8 text-3xl font-bold">註冊頁面 (施工中) </h1>;
+}
